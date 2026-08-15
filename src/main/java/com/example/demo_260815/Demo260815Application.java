8d65a34f1a4ff2de9fdfc5a43dd9e74416e50c08
ongsttt52 <ongsttt52@gmail.com>
2026-08-15 14:06:16 +0900

    initial commit

---
+++ src/main/java/com/example/demo_260815/Demo260815Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_260815;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo260815Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo260815Application.class, args);
+	}
+
+}
